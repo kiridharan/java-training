18b2a832a157b27ce66bee78eb41801f92a1268a
kiridharan <kiridharan@gmail.com>
2021-09-01 15:42:03 +0530

    training 1

---
+++ A1/Exercise1.java
@@ -0,0 +1,10 @@
+package A1;
+
+public class Exercise1 {
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!!!!!!!!!!!!!");
+
+	}
+
+}
---
+++ A1/Exercise2.java
@@ -0,0 +1,14 @@
+package A1;
+
+public class Exercise2 {
+	public static void main(String[] args) {
+		boolean a = false;
+		byte b = 12;
+		int c= 54;
+		float d = 57.2f;
+		double e = 52.3;
+		char f = 'K';
+		System.out.println(a + " " + b + " " + c + " " + d + " " + e + " " +f);
+	}
+
+}

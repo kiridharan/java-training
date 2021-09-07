5dcb05e1329ad85b5cdd542f05082f9b59a80510
kiridharan <kiridharan217dec2001@gmail.com>
2021-09-07 22:22:18 +0530

    update

--- Exercise_2/Kcg.java
+++ Exercise_2/Kcg.java
@@ -1,14 +1,6 @@
 package Exercise_2;
 
-public class Kcg
-{
-public static void main(String a[])
-{
-char a=’s’;
-int s=95;
-a=s;
-}
-}
+
 public class Kcg 
 {
 		public static void main(String a[]) 

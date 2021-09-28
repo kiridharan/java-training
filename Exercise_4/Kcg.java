9359a0d2b4b0cc9a6dcede45d5ed1c8ea99d6bdc
kiridharan <kiridharan217dec2001@gmail.com>
2021-09-28 22:21:34 +0530

    assignment 4

---
+++ Exercise_4/Kcg.java
@@ -0,0 +1,19 @@
+package Exercise_4;
+
+public class Kcg
+
+{
+
+public static void main(String a[]){
+
+for(int i = 0; i<5; i++);
+
+{
+
+System.out.println("KCG College of Technology");
+
+}
+
+}
+
+}
